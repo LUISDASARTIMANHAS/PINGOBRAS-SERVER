9fb09dab053ef3a43c2b7c3a89190954c586b64f
Glitch (pingobras-server) <none>
2022-11-10 23:06:47 +0000

    🍨🐑 Checkpoint ./package.json:26026384/5361 ./server.js:26026384/428

--- server.js
+++ server.js
@@ -45,22 +45,10 @@ fastify.get("/", function (request, reply) {
   let params = { seo: seo };
 
   // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo,
-    };
-  }
+  if (request.query.randomize) {}
 
   // The Handlebars code will be able to access the parameter values and build them into the page
-  return reply.view("/src/pages/index.hbs", params);
+  return reply.view("index.html", params);
 });
 
 /**
