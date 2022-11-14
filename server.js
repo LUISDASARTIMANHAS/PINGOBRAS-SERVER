1deb45438ad31bf04dc0a01da22ac91269d7ec09
Glitch (pingobras-server) <none>
2022-11-14 13:18:23 +0000

    🌴😽 Checkpoint ./server.js:26026384/1757 ./.gitignore:26026384/54 ./index.html:26026384/80

--- server.js
+++ server.js
@@ -8,16 +8,9 @@ const path = require("path");
 // Require the fastify framework and instantiate it
 const fastify = require("fastify")({
   // Set this to true for detailed logging:
-  logger: false,
+  logger: true,
 });
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
-
-// Setup our static files
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
-});
 
 // Formbody lets us parse incoming forms
 fastify.register(require("@fastify/formbody"));
@@ -30,69 +23,27 @@ fastify.register(require("@fastify/view"), {
 });
 
 // Load and parse SEO data
-const  seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
 
 /**
  * Our home page route
  *
  * Returns src/pages/index.hbs with data built into it
  */
-fastify.get("/", function (request, reply) {
+fastify.get("/", 
+function (request, reply) {
   // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-
-  // If someone clicked the option for a random color it'll be passed in the querystring
   if (request.query.randomize) {}
 
   // The Handlebars code will be able to access the parameter values and build them into the page
-  return reply.view("index.html", params);
+  return reply.view("index.html");
 });
 
-/**
- * Our POST route to handle and react to form submissions
- *
- * Accepts body data indicating the user choice
- */
-fastify.post("/", function (request, reply) {
-  // Build the params object to pass to the template
-  let params = { seo: seo };
+function Colors(){
+fastify.post("/",
+function (request, reply) {
 
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
-
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-
-    // Load our color data file
-    const colors = require("./src/colors.json");
-
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo,
-      };
-    } else {
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo,
-      };
-    }
-  }
-
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  return reply.view("index.html", params);
-});
+return reply.view("colors.html");
+});}
 
 // Run the server and report out to the logs
 fastify.listen(
